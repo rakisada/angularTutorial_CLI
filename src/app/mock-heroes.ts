d46f96f68ff34da0116e77f014e031592485c247
Rachel Akisada <rakisada@hotmail.com>
2017-11-06 16:00:52 -0600

    Added hero-detail, split code up more, property binding, input decorator, fixed UI so UL is centered

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import {Hero} from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Batman' },
+    { id: 12, name: 'Superman' },
+    { id: 13, name: 'The Flash' },
+    { id: 14, name: 'Thor' },
+    { id: 15, name: 'The Hulk' },
+    { id: 16, name: 'Wonder Woman' },
+    { id: 17, name: 'Star Lord' },
+    { id: 18, name: 'Dr. Strange' },
+    { id: 19, name: 'Iron Man' },
+    { id: 20, name: 'Captain America' }
+]
