1d8caaf72ab606f086d906ffe1c442a290e6cba1
Rachel Akisada <rakisada@hotmail.com>
2017-11-07 11:00:58 -0600

    Refactored data into HeroService class, created messageService to return message

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -8,15 +10,20 @@ import { HEROES } from '../mock-heroes';
   //encapsulation: ViewEncapsulation.None
 })
 export class HeroesComponent implements OnInit {
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
 
-  constructor() { }
+  constructor(private heroSerivce: HeroService) { }
   
   ngOnInit() {
-    
+    this.getHeroes();
   }
   
+  getHeroes(): void {
+    this.heroSerivce.getHeroes()
+    .subscribe(heroes => this.heroes = heroes);
+  }
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
